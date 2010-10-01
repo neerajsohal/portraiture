621487efb9138f44c0512b0e182a6931293c44e0
codemastersnake <codemaster.snake@gmail.com>
2010-10-01 16:18:26 +0530

    adding untracked files

---
+++ includes/dashboard_widgets.php
@@ -0,0 +1,38 @@
+<?php
+//// Create the function to output the contents of our Dashboard Widget
+
+function portraiture_dashboard_widget_function() {
+	portraiture_feed();
+}
+
+// Create the function use in the action hook
+
+function portraiture_add_dashboard_widgets() {
+	wp_add_dashboard_widget('portraiture_dashboard_widget', 'Portraiture Theme News', 'portraiture_dashboard_widget_function');
+
+	// Globalize the metaboxes array, this holds all the widgets for wp-admin
+
+	global $wp_meta_boxes;
+
+	// Get the regular dashboard widgets array
+	// (which has our new widget already but at the end)
+
+	$normal_dashboard = $wp_meta_boxes['dashboard']['normal']['core'];
+
+	// Backup and delete our new dashbaord widget from the end of the array
+
+	$portraiture_widget_backup = array('portraiture_dashboard_widget' => $normal_dashboard['portraiture_dashboard_widget']);
+	unset($normal_dashboard['portraiture_widget_backup']);
+
+	// Merge the two arrays together so our widget is at the beginning
+
+	$sorted_dashboard = array_merge($portraiture_widget_backup, $normal_dashboard);
+
+	// Save the sorted array back into the original metaboxes
+
+	$wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dashboard;
+}
+
+// Hook into the 'wp_dashboard_setup' action to register our other functions
+
+add_action('wp_dashboard_setup', 'portraiture_add_dashboard_widgets' );
---
+++ lib/helper.php
@@ -0,0 +1,40 @@
+<?php
+
+function twitter_feed($username, $num = 5) {
+
+	$rssUrl = "http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=".$username."&count=".$num;
+	$rss = @file_get_contents($rssUrl);
+
+	if($rss) {
+		$xml = @simplexml_load_string($rss);
+		if($xml !== false) {
+			foreach($xml->channel->item as $tweet) {
+				echo '<p>'.str_replace($username . ': ', '', $tweet->description) . "<br/>";
+				echo  "</p>";
+			}
+		}
+	}
+}
+
+function portraiture_feed($num = 5) {
+	$rssUrl = "http://feeds.feedburner.com/wp-portraiture";
+	$rss = @file_get_contents($rssUrl);
+	if($rss) {
+		$xml = @simplexml_load_string($rss);
+		if($xml !== false) {
+            $ctr = 0;
+			echo "<div class='rss-widget'>";
+			echo "<ul>";                
+			foreach($xml->channel->item as $post) {
+                if($ctr++ > 4) break;
+                
+                echo '<li>';
+				echo '<a class="rsswidget">'.$post->title.'</a> ';
+                echo '<span class="rss-date">'. date('M d, Y',strtotime($post->pubDate)).'</span>';
+                echo '<div class="rssSumarry">'.$post->description.'</div>';
+                echo '</li>';
+			}
+			echo "</ul></div>";
+		}
+	}
+}
