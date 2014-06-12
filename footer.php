9f7c37fb8f1a325c0b2dc00f5d0454731b78158e
neerajsohal <hello@neerajkumar.name>
2014-06-12 15:57:45 +0530

    preping release

--- footer.php
+++ footer.php
@@ -8,6 +8,8 @@
 	</div>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>
+		<script type="text/javascript" src="site.js"></script>
 		<?php wp_footer(); ?>
 	</body>
 </html>
