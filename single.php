8015e97dd49f2bc16bf9b2bafc45ee02da68a9e2
neerajsohal <hello@neerajkumar.name>
2014-10-27 14:18:55 +0530

    adding comments template

--- single.php
+++ single.php
@@ -1,28 +1,25 @@
 <?php get_header('single'); ?>
-		<div class="container">
-			<?php
-			if (have_posts()) :
-				while (have_posts()) : the_post(); ?>
-			<div class="row">
-				<div class="col-lg-12 col-md-12">
+	<div class="container">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div class="row">
+			<div class="col-lg-12 col-md-12">
+				<div class="post <?php post_class(); ?>">
+					<div class="content">
+						<?php echo apply_filters('the_content', get_the_content()); ?>
+					</div>
 					
-					<div class="post <?php post_class(); ?>">
-						<div class="content">
-							<?php echo apply_filters('the_content', get_the_content()); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-			<?php
-				endwhile;
-			endif;
-			?>
-			<div class="row">
-				<div class="col-lg-12 col-md-12">
-					<div class="page_nav_links">
-						<p><small><?php posts_nav_link(); ?></small></p>
-					</div>
+					<?php comments_template(); ?>
+					
 				</div>
 			</div>
 		</div>
+		<?php endwhile; endif; ?>
+		<div class="row">
+			<div class="col-lg-12 col-md-12">
+				<div class="page_nav_links">
+					<p><small><?php posts_nav_link(); ?></small></p>
+				</div>
+			</div>
+		</div>
+	</div>
 <?php get_footer('single'); ?>
